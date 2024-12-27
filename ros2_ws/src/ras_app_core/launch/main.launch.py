d38609d5ce8ddfeaae8bc600b4359a73e7430e85
Sachin Kumar <sachin.kumar@accelerationrobotics.com>
2024-12-27 14:28:43 +0100

    intermediate gripper server node added

--- ros2_ws/src/ras_app_core/launch/main.launch.py
+++ ros2_ws/src/ras_app_core/launch/main.launch.py
@@ -5,10 +5,18 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 import logging
+from launch_ros.actions import Node
 
 def generate_launch_description():
     RasObject.init()
     launch = None
+    gripper_node = Node(
+            package='ras_app_core',
+            executable='intermediate_gripper_server.py',
+            name='intermediate_gripper_server',
+            output='screen',
+            parameters=[{'use_real_gripper': True if RasObject.ras.real.mode == "real" else False}]
+        )
     logger = logging.getLogger('launch')
 
     if(RasObject.ras.real.mode == "real"):
@@ -26,4 +34,4 @@ def generate_launch_description():
     else:
         logger.error("Invalid mode")
 
-    return LaunchDescription([launch])
+    return LaunchDescription([launch, gripper_node])
