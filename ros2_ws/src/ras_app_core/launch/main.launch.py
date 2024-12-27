4853a951aa82cd20f3e85e0a2627c3663dd91250
Sachin Kumar <sachin.kumar@accelerationrobotics.com>
2024-12-27 13:55:36 +0100

    main launch file updated to start sim/real launch file based on config

--- ros2_ws/src/ras_app_core/launch/main.launch.py
+++ ros2_ws/src/ras_app_core/launch/main.launch.py
@@ -1,11 +1,29 @@
-from ras_common.ras_common.config.loaders.ras_config import RasObject
+from ras_common.config.loaders.ras_config import RasObject
+
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+import logging
 
 def generate_launch_description():
     RasObject.init()
-    if(RasObject.ras.real.mode == "real"):
-        print("Real mode")
-    else:
-        print("Sim mode")
+    launch = None
+    logger = logging.getLogger('launch')
 
-    return LaunchDescription()
+    if(RasObject.ras.real.mode == "real"):
+        logger.info('Real mode')
+        launch = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([FindPackageShare('ras_app_core'), '/launch', '/real.launch.py'])
+        )
+
+    elif(RasObject.ras.real.mode == "sim"):
+        logger.info("Sim mode")
+        launch = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([FindPackageShare('ras_app_core'), '/launch', '/sim.launch.py'])
+        )
+
+    else:
+        logger.error("Invalid mode")
+
+    return LaunchDescription([launch])
