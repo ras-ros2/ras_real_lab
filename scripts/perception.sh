be5dbf58c8dfb4be852b6e0f4c52884978da652c
Harsh Davda <harsh.davda11@outlook.com>
2025-01-09 21:05:11 +0530

    rm perception script

--- scripts/perception.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-tmux new-session -d -s perception_session
-
-tmux send-keys -t perception_session:0.0 "ros2 run ras_perception aruco_detection.py" C-m
-
-tmux attach-session -t perception_session
