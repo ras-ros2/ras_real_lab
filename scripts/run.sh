d3cb59eb12708db6bc3ae04e3c26865b8f387ba0
noemoji041 <aloksharma041@gmail.com>
2025-01-09 18:50:57 +0530

    run.sh and perception.sh modified

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,14 +10,16 @@ tmux send-keys -t main_session:0.2 "ros2 run ras_bt_framework TrajectoryRecordsS
 
 tmux new-window -t main_session:1 -n 'robot'
 tmux split-window -v -t main_session:1
+tmux split-window -v -t main_session:1
 tmux send-keys -t main_session:1.0 "ros2 launch ras_app_core sim.launch.py" C-m
 tmux send-keys -t main_session:1.1 "ros2 run ras_aws_transport ftp_server.py" C-m
+tmux send-keys -t main_session:1.2 "ros2 run ras_bt_framework executor" C-m
 
 tmux new-window -t main_session:2 -n 'logging'
 tmux split-window -v -t main_session:2
 tmux split-window -v -t main_session:2
 tmux send-keys -t main_session:2.0 "ros2 run ras_aws_transport log_sender.py" C-m
-tmux send-keys -t main_session:2.1 "ros2 run ras_bt_framework executor" C-m
-tmux send-keys -t main_session:2.2 "ros2 run ras_bt_framework FakeGripperServer.py" C-m
+tmux send-keys -t main_session:2.1 "ros2 run ras_bt_framework FakeGripperServer.py" C-m
+tmux send-keys -t main_session:2.2 "ros2 run ras_bt_framework logging_manager.py" C-m
 
 tmux attach-session -t main_session
