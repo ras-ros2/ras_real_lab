ee658a503d3eded88770b634c4ff356dc36b4e72
Harsh Davda <harsh.davda11@outlook.com>
2025-01-14 02:42:51 +0530

    mode fixes

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,10 +5,16 @@ export RAS_WORKSPACE_PATH=$RAS_APP_PATH/ros2_ws
 export IGN_GAZEBO_RESOURCE_PATH=$RAS_APP_PATH/assets/objects/old_models:$RAS_APP_PATH/third_party/gazebo_models:${IGN_GAZEBO_RESOURCE_PATH}
 export RAS_ROBOT_MODE=sim
 export RAS_MODE_SCRIPT=$RAS_APP_PATH/scripts/real_mode.bashrc
-source $RAS_MODE_SCRIPT
 source $RAS_WORKSPACE_PATH/install/setup.bash
 export IGN_PARTITION=$RAS_APP_NAME
 export ROS_DOMAIN_ID=1
+
+source_mode_script(){
+    if [ -e $RAS_MODE_SCRIPT ]; then
+        source $RAS_MODE_SCRIPT
+    fi
+}
+source_mode_script
 
 ras_app() { if [ -e /tmp/.RAS_RUN ]
     then
@@ -22,7 +28,7 @@ ras_app() { if [ -e /tmp/.RAS_RUN ]
             echo "Error: Invalid mode. Use 'sim' or 'real'."
             exit 1
         fi
-        source $RAS_MODE_SCRIPT
+        source_mode_script
         echo Starting App in $RAS_ROBOT_MODE mode
         touch /tmp/.RAS_RUN
         $RAS_APP_PATH/scripts/run.sh
