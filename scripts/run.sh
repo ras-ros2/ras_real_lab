b56bf7765dc235efca6cf38a65aa639bd2adfa41
Harsh Davda <harsh.davda11@outlook.com>
2025-02-10 09:49:40 +0530

    removed xarm references

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,7 +12,7 @@ tmux send-keys -t main_session:0.2 "ros2 run ras_bt_framework TrajectoryRecordsS
 tmux new-window -t main_session:1 -n 'robot'
 tmux split-window -v -t main_session:1
 tmux split-window -v -t main_session:1
-tmux send-keys -t main_session:1.0 "ros2 launch ras_app_main sim.launch.py" C-m
+tmux send-keys -t main_session:1.0 "ros2 launch ras_app_main main.launch.py" C-m
 # tmux send-keys -t main_session:1.1 "ros2 run ras_transport ftp_server.py" C-m
 tmux send-keys -t main_session:1.2 "ros2 run ras_bt_framework executor" C-m
 
@@ -20,7 +20,8 @@ tmux new-window -t main_session:2 -n 'logging'
 tmux split-window -v -t main_session:2
 tmux split-window -v -t main_session:2
 # tmux send-keys -t main_session:2.0 "ros2 run ras_transport log_sender.py" C-m
-tmux send-keys -t main_session:2.1 "ros2 run ras_bt_framework FakeGripperServer.py" C-m
+# tmux send-keys -t main_session:2.1 "ros2 run ras_bt_framework FakeGripperServer.py" C-m
+# tmux send-keys -t main_session:2.1 "ros2 run ras_transport gripper.py" C-m
 tmux send-keys -t main_session:2.2 "ros2 run ras_bt_framework logging_manager.py" C-m
 
 tmux new-window -t main_session:3 -n 'debugging'
@@ -29,7 +30,7 @@ tmux split-window -v -t main_session:3
 tmux split-window -v -t main_session:3
 tmux split-window -v -t main_session:3
 
-tmux send-keys -t main_session:3.0 "ros2 run ras_perception aruco_detection.py" C-m
+# tmux send-keys -t main_session:3.0 "ros2 run ras_perception aruco_detection.py" C-m
 tmux send-keys -t main_session:3.1 "ros2 run ras_bt_framework dummy_logging_server.py" C-m
 # tmux send-keys -t main_session:3.2 "ros2 run ras_transport transport_robot_service.py" C-m
 
